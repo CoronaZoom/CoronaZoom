479aa567a802a08fc5220fd69776e7a314cb5584
bigwon9999 <64629140+bigwon9999@users.noreply.github.com>
2020-05-07 16:57:02 +0900

    Add files via upload

--- js/getCurrentPosition.js
+++ js/getCurrentPosition.js
@@ -1,18 +1,16 @@
-<button id="getLocation" type="button">위치 정보 수집</button>
-<div id="map" style="width:500px; height: 500px;display: block;"></div>
-<script> (function ()
-{ let map, infoWindow; window.initMap = function ()
-  { map = new google.maps.Map(document.getElementById('map'),
-  { center: {lat: -34.397, lng: 150.644}, zoom: 8 });
-  infoWindow = new google.maps.InfoWindow({map: map});
-  infoWindow.setContent('Your location'); };
-  function successCallback(position)
-  { let pos = { lat: position.coords.latitude, lng: position.coords.longitude };
-   infoWindow.setPosition(pos); map.setCenter(pos);
-   alert("Your current position is: latitude(" + pos.lat + "), longitude(" + pos.lng + ")"); }
-   function errorCallback(error)
-   { alert("Error: " + error.message); }
-   document.getElementById("getLocation").onclick = function ()
-   { navigator.geolocation.getCurrentPosition(successCallback, errorCallback); }; }());
-   </script>
-   <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=p8qdphtxe4" async="" defer=""></script>
+function getLocation() {
+  if (navigator.geolocation) { // GPS를 지원하면
+    navigator.geolocation.getCurrentPosition(function(position) {
+      alert(position.coords.latitude + ' ' + position.coords.longitude);
+    }, function(error) {
+      console.error(error);
+    }, {
+      enableHighAccuracy: false,
+      maximumAge: 0,
+      timeout: Infinity
+    });
+  } else {
+    alert('GPS를 지원하지 않습니다');
+  }
+}
+getLocation();
