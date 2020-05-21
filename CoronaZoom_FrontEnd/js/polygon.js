a9e9e2ba2b8eb1e2dac6a196beccc8617ce63169
jsh5408 <51473971+jsh5408@users.noreply.github.com>
2020-05-21 22:50:16 +0900

    Add files via upload

---
+++ CoronaZoom_FrontEnd/js/polygon.js
@@ -0,0 +1,101 @@
+var HOME_PATH = window.HOME_PATH || '.',
+    urlPrefix = HOME_PATH +'/data/region',
+    urlSuffix = '.json',
+    regionGeoJson = [],
+    loadCount = 0;
+
+var map = new naver.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    mapTypeId: 'normal',
+    center: new naver.maps.LatLng(36.4203004, 128.317960)
+});
+
+// alert('성공');
+
+naver.maps.Event.once(map, 'init_stylemap', function () {
+    for (var i = 1; i < 18; i++) {
+        var keyword = i +'';
+
+        if (keyword.length === 1) {
+            keyword = '0'+ keyword;
+        }
+
+        $.ajax({
+            url: 'https://raw.githubusercontent.com/4z7l/CoronaZoom_WEB/master/CoronaZoom_FrontEnd/testdata/region'+keyword+urlSuffix,   //urlPrefix + keyword + urlSuffix
+            dataType: 'json',
+            success: function(idx) {
+                return function(geojson) {
+                    regionGeoJson[idx] = geojson;
+
+                    loadCount++;
+
+                    if (loadCount === 17) {
+                        startDataLayer();
+                    }
+                }
+            }(i - 1)
+        });
+    }
+});
+
+var tooltip = $('<div style="position:absolute;z-index:1000;padding:5px 10px;background-color:#fff;border:solid 2px #000;font-size:14px;pointer-events:none;display:none;"></div>');
+
+tooltip.appendTo(map.getPanes().floatPane);
+
+function startDataLayer() {
+    map.data.setStyle(function(feature) {
+        var styleOptions = {
+            fillColor: '#ff0000',
+            fillOpacity: 0.0001,
+            strokeColor: '#ff0000',
+            strokeWeight: 2,
+            strokeOpacity: 0.4
+        };
+
+        if (feature.getProperty('focus')) {
+            styleOptions.fillOpacity = 0.6;
+            styleOptions.fillColor = '#0f0';
+            styleOptions.strokeColor = '#0f0';
+            styleOptions.strokeWeight = 4;
+            styleOptions.strokeOpacity = 1;
+        }
+
+        return styleOptions;
+    });
+
+    regionGeoJson.forEach(function(geojson) {
+        map.data.addGeoJson(geojson);
+    });
+
+    map.data.addListener('click', function(e) {
+        var feature = e.feature;
+
+        if (feature.getProperty('focus') !== true) {
+            feature.setProperty('focus', true);
+        } else {
+            feature.setProperty('focus', false);
+        }
+    });
+
+    map.data.addListener('mouseover', function(e) {
+        var feature = e.feature,
+            regionName = feature.getProperty('area1');
+
+        tooltip.css({
+            display: '',
+            left: e.offset.x,
+            top: e.offset.y
+        }).text(regionName);
+
+        map.data.overrideStyle(feature, {
+            fillOpacity: 0.6,
+            strokeWeight: 4,
+            strokeOpacity: 1
+        });
+    });
+
+    map.data.addListener('mouseout', function(e) {
+        tooltip.hide().empty();
+        map.data.revertStyle();
+    });
+}
