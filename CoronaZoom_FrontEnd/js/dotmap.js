17cc89661ecf13b099faff8aa05501b464bf6639
jsh5408 <51473971+jsh5408@users.noreply.github.com>
2020-05-29 00:01:14 +0900

    Update dotmap.js

--- CoronaZoom_FrontEnd/js/dotmap.js
+++ CoronaZoom_FrontEnd/js/dotmap.js
@@ -8,14 +8,28 @@ var map = new naver.maps.Map(mapDiv, {
 //ajax...
 naver.maps.Event.once(map, 'init_stylemap', function() {
   $.ajax({
-    url: 'https://raw.githubusercontent.com/4z7l/CoronaZoom_WEB/master/CoronaZoom_FrontEnd/testdata/earthquake.json',                                                    dataType: 'json',
+    url: 'http://ec2-13-125-253-144.ap-northeast-2.compute.amazonaws.com:3000/api/ConfirmerVisitedPlaces',                                                    dataType: 'json',
     method:"get",
+    crossOrigin: true,
     success: startDotMap
   });
 });
 function startDotMap(data) {
+  var listLen = data.length;
+  console.log("파싱성공");  // 디버깅 용
+  
+  // 네이버 earthquake.js의 coordinates 형태랑 같게 저장하기 위해 2차원 배열 arr 사용
+  // arr[][x,y] 형태
+  var x, y;
+  var arr = new Array();
+  var test = [x, y];
+  for(var i=0;i<listLen;i++){
+    test = [data[i]['Lat'], data[i]['Lon']];
+    arr[i] = test;
+    //console.log(test);  // 어떻게 저장되는지 확인
+  }
   var dotmap = new naver.maps.visualization.DotMap({
     map: map,
-    data: data.coordinates
-    });
-  }
+    data: arr
+  });
+}
