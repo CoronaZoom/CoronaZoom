b95b3e3a8c3d8ac5307cc4d0e81ec93fe19c0375
bigwon9999 <64629140+bigwon9999@users.noreply.github.com>
2020-06-04 14:51:53 +0900

    Update dotmap.js

--- CoronaZoom_FrontEnd/js/dotmap.js
+++ CoronaZoom_FrontEnd/js/dotmap.js
@@ -8,7 +8,7 @@ var map = new naver.maps.Map(mapDiv, {
 //ajax...
 naver.maps.Event.once(map, 'init_stylemap', function() {
   $.ajax({
-    url: 'http://ec2-13-125-253-144.ap-northeast-2.compute.amazonaws.com:3000/api/ConfirmerVisitedPlaces',                                                    dataType: 'json',
+    url: 'http://ec2-13-125-253-144.ap-northeast-2.compute.amazonaws.com:3000/api/ConfirmerInfo',                                                    dataType: 'json',
     method:"get",
     crossOrigin: true,
     success: startDotMap
@@ -17,14 +17,14 @@ naver.maps.Event.once(map, 'init_stylemap', function() {
 function startDotMap(data) {
   var listLen = data.length;
   console.log("파싱성공");  // 디버깅 용
-  
+
   // 네이버 earthquake.js의 coordinates 형태랑 같게 저장하기 위해 2차원 배열 arr 사용
   // arr[][x,y] 형태
   var x, y;
   var arr = new Array();
   var test = [x, y];
   for(var i=0;i<listLen;i++){
-    test = [data[i]['Lat'], data[i]['Lon']];
+    test = [data[i]['Latitude'], data[i]['Longitude']];
     arr[i] = test;
     //console.log(test);  // 어떻게 저장되는지 확인
   }
